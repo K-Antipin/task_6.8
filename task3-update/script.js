c273ac1c7710b456e7f55266735cf7f736d4efae
K-Abtipin <114164442+K-Abtipin@users.noreply.github.com>
2023-01-06 15:25:21 +0300

    update

--- task3-update/script.js
+++ task3-update/script.js
@@ -1,25 +1,7 @@
-const trafficLightEl = document.querySelector('.trafficLight');
-// const trafficLightAuto = document.querySelector('.trafficLightAuto');
-
-function makeGreen() {
-    trafficLightEl.childNodes[5].style.background = 'green';
-    trafficLightEl.removeEventListener('click', makeGreen);
-    trafficLightEl.addEventListener('click', makeYellow);
-    trafficLightEl.childNodes[1].style.background = 'black';
-}
-
-function makeYellow() {
-    trafficLightEl.childNodes[3].style.background = 'yellow';
-    trafficLightEl.removeEventListener('click', makeYellow);
-    trafficLightEl.addEventListener('click', makeRed);
-    trafficLightEl.childNodes[5].style.background = 'black';
-}
-
-function makeRed() {
-    trafficLightEl.childNodes[1].style.background = 'red';
-    trafficLightEl.removeEventListener('click', makeRed);
-    trafficLightEl.addEventListener('click', makeGreen);
-    trafficLightEl.childNodes[3].style.background = 'black';
-}
-
-trafficLightEl.addEventListener('click', makeGreen);
+let i = 0;
+let color = ['red','yellow','green'];
+document.querySelector('.trafficLight').addEventListener('click', (event) => {
+    event.target.children[i].style.background = 'black';
+    i = i == 0 ? 2 : --i;
+    event.target.children[i].style.background = color[i];
+});
--- task5/script.js
+++ task5/script.js
@@ -6,7 +6,7 @@ textInput.addEventListener('keypress', (event) => {
     text.textContent = text.textContent + event.key;
 });
 
-form.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(text.textContent);
     text.textContent = '';
